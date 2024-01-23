943c8021bfe14ba0c2ca3b1a133f027b5394ec2f
Holbiwan <sabrinapapeau@gmail.com>
2024-01-23 14:35:26 +0100

    Update code

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else if (response.statusCode === 200){
+  } else if (response && response.statusCode === 200) {
     console.log(JSON.parse(body).title);
   } else {
-      console.log('Error occured. Status code: ' + response.statusCode);
-    }
-  });
+    console.log('Error occurred. Status code: ' + (response ? response.statusCode : 'Unknown'));
+  }
+});
