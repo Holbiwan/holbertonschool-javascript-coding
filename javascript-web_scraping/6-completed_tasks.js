7fbdc9d717c919a5de1404e260509d91ac21a3a4
Holbiwan <sabrinapapeau@gmail.com>
2024-01-25 11:21:01 +0100

    Fix code issue

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+
+  const tasks = JSON.parse(body);
+  const results = {};
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (!results[task.userId]) {
+        results[task.userId] = 0;
+      }
+      results[task.userId]++;
+    }
+  });
+
+  console.log(results);
 });
