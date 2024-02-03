b6088724b75d94e41b6e44b00cfc01d2b9a9be08
Holbiwan <sabrinapapeau@gmail.com>
2024-02-03 12:34:57 +0100

    Create utils.js file

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+async function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const lines = data.trim().split('\n');
+        const headers = lines[0].split(',');
+        const students = [];
+
+        for (let i = 1; i < lines.length; i += 1) {
+          const values = lines[i].split(',');
+          const student = {};
+
+          for (let j = 0; j < headers.length; j += 1) {
+            student[headers[j]] = values[j];
+          }
+
+          students.push(student);
+        }
+
+        resolve(students);
+      }
+    });
+  });
+}
+
+module.exports = { readDatabase };
