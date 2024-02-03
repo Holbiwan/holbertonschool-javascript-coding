f3842b1ae78ba35b1c8a2a5c9357e72d44fefa85
Holbiwan <sabrinapapeau@gmail.com>
2024-02-03 02:17:54 +0100

    Check errors and syntax

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,36 @@
 const fs = require('fs');
 
-const countStudents = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const rows = data.trim().split('\n').filter((element) => element.length > 0);
-
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, (error, csvData) => {
+    if (error) {
+      reject(Error('Cannot load the database'));
+    }
+    if (csvData) {
       const fields = {};
-      rows.forEach((element) => {
-        const row = element.split(',');
-        if (row[3] in fields) {
-          fields[row[3]].push(row[0]);
-        } else {
-          fields[row[3]] = [row[0]];
+      let data = csvData.toString().split('\n');
+      data = data.filter((element) => element.length > 0);
+      data.shift();
+
+      data.forEach((element) => {
+        if (element.length > 0) {
+          const row = element.split(',');
+          if (row[3] in fields) {
+            fields[row[3]].push(row[0]);
+          } else {
+            fields[row[3]] = [row[0]];
+          }
         }
       });
-
-      const totalStudents = rows.length;
-      console.log(`Number of students: ${totalStudents}`);
-
+      console.log(`Number of students: ${data.length}`);
       for (const field in fields) {
         if (field) {
           const list = fields[field];
-          console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.toString().replace(/,/g, ', ')}`);
         }
       }
-
-      resolve();
-    });
+    }
+    resolve();
   });
-};
+});
 
 module.exports = countStudents;
