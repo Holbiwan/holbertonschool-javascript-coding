b0ca6192f44061bd029da05cf53dfc4a19689082
HOLBIWAN (Saby) <sabrinapapeau@gmail.com>
2024-02-03 18:23:05 +0100

    Change new error message  content in Update utils.js file

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -4,7 +4,7 @@ async function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
-        reject(err);
+        reject(new Error('Cannot load the database'));
       } else {
         const lines = data.trim().split('\n');
         const headers = lines[0].split(',');
