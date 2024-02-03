0799d52212dd9f0228b89916b0cf1488cb83b62b
Holbiwan <sabrinapapeau@gmail.com>
2024-02-03 14:44:04 +0100

    Write the server reusing everything created

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import mapRoutes from './routes';
+
+const app = express();
+const port = 1245;
+
+mapRoutes(app);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
